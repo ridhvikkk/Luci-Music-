87dfa2daf5c290ea5f25982947cb9af9cd35e646
ridhvikkk <tanmayshikha@gmail.com>
2024-06-14 14:24:54 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,44 +1,21 @@
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
-
 const { EmbedBuilder } = require('discord.js');
-
 
 module.exports = {
   name: "help",
-  description: "Get information about the bot",
+  description: "Get a list of my commands",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
     try {
-     
 
-      const embed = new EmbedBuilder()
-         .setColor('#0099ff')
-      .setTitle('Luci Music')
-      .setDescription('Thank you for choosing Luci Music!\n\n- Here are the available commands:\n\n' +
-        '**/play :** Start playing the songs.\n' +
-        '**/ping :** Check bot latency.\n' +
-        '**/support :** Display support server info.\n' +
-        '**/features :** Compare how this bot is better than other music bots.);
+        const embed = new EmbedBuilder()
+            .setColor('#b300ff')
+            .setAuthor({
+              name: 'Commands',
+              iconURL: 'https://cdn3.emoji.gg/emojis/7215_thonk.png',
+          })
+            .setImage('https://media.discordapp.net/attachments/1250810751573299312/1251096894948638813/image.png?ex=666d5648&is=666c04c8&hm=4ce19a28f1a70441832de7a2c104ea0355538e2475de4efa80ffca9fc8c6fb4b&=&format=webp&quality=lossless&width=660&height=258')
+            .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
     } catch (e) {
@@ -46,24 +23,3 @@ module.exports = {
   }
   },
 };
-
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
