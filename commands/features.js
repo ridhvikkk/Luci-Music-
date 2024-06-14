c81cf36ed264d1878e4eb05facdca4ed6751d425
ridhvikkk <tanmayshikha@gmail.com>
2024-06-14 13:52:15 +0530

    Update features.js

--- commands/features.js
+++ commands/features.js
@@ -1,44 +1,22 @@
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
-
 const { EmbedBuilder } = require('discord.js');
-
 
 module.exports = {
   name: "features",
-  description: "Know about the features of the bot",
+  description: "Why this music bot?",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
     try {
-     
 
-      const embed = new EmbedBuilder()
-         .setColor('#0099ff')
-      .setTitle('Features of Luci Music Bot')
-      .setDescription('Thank you for choosing Luci Music!\n\n- Here are the features that this bot offers for free unlike other music bots.\n\n' +
-        '**1. :** If a playlist is queued, the bot will remain in the voice channel until the playlist ends, indirectly, 24x7. \n' +
-        '**2. :** The bot has an easy button interface, no hustle to type the commands again and again for skipping, queueing etc.\n' +
-        '**3. :** The bot supports 3 music platforms: Spotify, Sound Cloud, YouTube. \n' +
-        '**4. :** Enjoy music with 8D sound quality and minimal lagging.);
+        const embed = new EmbedBuilder()
+            .setColor('#b300ff')
+            .setAuthor({
+              name: 'Features',
+              iconURL: 'https://cdn3.emoji.gg/emojis/7215_thonk.png',
+          })
+            .setDescription(`**Reasons to choose me over other music bots**`)
+            .setImage('https://cdn.discordapp.com/attachments/1250810751573299312/1251088161057280041/image.png?ex=666d4e25&is=666bfca5&hm=2b434e940ba76bd301bd70438efd777889f50ecce8d796dd94a807b6fdfbdca4&')
+            .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
     } catch (e) {
@@ -46,24 +24,3 @@ module.exports = {
   }
   },
 };
-
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
