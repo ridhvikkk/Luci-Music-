2004e76066b711714aed1cbf43ef252d5d5773e8
ridhvikkk <tanmayshikha@gmail.com>
2024-07-01 17:01:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 const config = require('./config.js');
 require("./bot.js");
 
-//server join/leave logging
-client.on(Events.GuildCreate, async guild -> {
+//server join
+client.on(Events.GuildCreate, async guild => {
 const channel = await client.channels.cache.get('1256955078695190647');
 const name = guild.name;
 const memberCount = guild.memberCount;
-const owner
+const owner = guild.ownerId;
+const owner = await client.users.cache.get(ownerID);
+const ownerName = owner.username;
+
 const embed = new EmbedBuilder()
 .setColor("Green")
+.setTitle('New Server Joined')
+.addFields({ nme: 'Server Name', value: `> ${name}`})
+.addFields({ nme: 'Server Members', value: `> ${memberCount}`})
+.addFields({ nme: 'Server Owner', value: `> ${owner}`})
+.addFields({ nme: 'Server Age', value: `> <t:${parseInt(guild.createdTimestamp / 1000)}:R>`})
+.setTimestamp()
+.setFooter({ text: 'Guild Join'})
+
+await channel.send({ embed: [embed] });
+
+//server left
+client.on(Events.GuildDelete async guild => {
+const channel = await client.channels.cache.get('1256955078695190647');
+const name = guild.name;
+const memberCount = guild.memberCount;
+const owner = guild.ownerId;
+const owner = await client.users.cache.get(ownerID);
+const ownerName = owner.username;
+
+const embed = new EmbedBuilder()
+.setColor("Green")
+.setTitle('Server left! :(')
+.addFields({ nme: 'Server Name', value: `> ${name}`})
+.addFields({ nme: 'Server Members', value: `> ${memberCount}`})
+.addFields({ nme: 'Server Owner', value: `> ${owner}`})
+.addFields({ nme: 'Server Age', value: `> <t:${parseInt(guild.createdTimestamp / 1000)}:R>`})
+.setTimestamp()
+.setFooter({ text: 'Guild Leave'})
+
+await channel.send({ embed: [embed] });
+
+
 
   /*
 
