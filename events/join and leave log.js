71ea079442308a62936c58ff6666e8910d144d1c
ridhvikkk <tanmayshikha@gmail.com>
2024-07-03 18:21:09 +0530

    Update join and leave log.js

--- events/join and leave log.js
+++ events/join and leave log.js
@@ -18,11 +18,12 @@ const clientGuildCount = await client.guilds.cache.size;
 const embed = new EmbedBuilder()
 .setColor("Green")
 .setTitle('**New Server Joined**')
-.addFields({ nme: 'Server Name', value: `\`${name}\``})
-.addFields({ nme: 'Server ID', value: `> ${id}`})
-.addFields({ nme: 'Server Owner', value: `> ${owner.user.username} ($(owner.id})\``})
-.addFields({ nme: 'Server Member Count', value: `\`${memberCount}\``})
+.addFields({ name: 'Server Name', value: `\`${name}\``})
+.addFields({ name: 'Server ID', value: `> ${id}`})
+.addFields({ name: 'Server Owner', value: `> ${owner.user.username} ($(owner.id})\``})
+.addFields({ name: 'Server Member Count', value: `\`${memberCount}\``})
 .setDescription(`This is my \`${clientGuildCount}\` server that I am in. Use the button below to fetch the invite link to this guild.`)
+.setFooter({ text: `Server Join Logs.`})
 .setTimestamp();
 
 const button = new ButtonBuilder()
@@ -51,7 +52,16 @@ collector.on('collect', async i => {
       channel = c;
       break;
     }
+    
     if (!channel) return await i.reply({ content: `:( Sorry, I couldn't find any channels in this server to create an invite to the server.`, ephemeral: true});
+    
     const invite = await channel.createInvite().catch(err => {})
+    await i.reply({ content: `Heres the invite to ${name}: https://discord.gg/${invite.code}`, ephemeral:true})
   }
-})
+});
+
+collector.on('end', async () => {
+  button.setDisabled(true);
+  embed.setFooter({ text: `Server Join Logs. -- the invite fetch has expired`
+  await msg.edit({ embeds: [embed], components:[row]})                 
+});
